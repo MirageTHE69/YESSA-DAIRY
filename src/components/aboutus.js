daa8c2dbf3d896d2a620a605b970247dd3aea6a4
unknown <mthakore12@gmail.com>
2024-04-08 22:31:39 +0530

    all good finally

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,113 +1,41 @@
-import React, { useState } from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
-import LogoSvg from '../images/logo.svg';
-import InstagramIcon from '../images/instagram.svg';
-import FacebookIcon from '../images/facebook.svg';
-import LinkedInIcon from '../images/linkedin.svg';
-import '../styles/fonts.css';
+import React from "react"
+import Frame from "../images/Frame.svg"
 
-function Navbar() {
-    const [showMenu, setShowMenu] = useState(false);
-
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-    };
-
-    const closeMenu = () => {
-        setShowMenu(false);
-    };
-
-    return (
-        <nav className="flex flex-row justify-between items-center h-auto sm:h-14 bg-white text-white px-8 sm:px-16 py-4">
-            {/* Logo Section */}
-            <div className="flex items-start w-full sm:w-auto mt-5">
-                <img src={LogoSvg} alt="Logo" className="h-10 sm:h-16" />
-                {/* Toggle button for small screens */}
-                <button onClick={toggleMenu} className="ml-auto sm:hidden block text-black hover:text-green-500 focus:outline-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fl" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        {showMenu ? (
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                        ) : (
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-                        )}
-                    </svg>
-                </button>
-            </div>
-
-            {/* Menu Items */}
-            <ul className={`flex flex-col mt-5 sm:flex sm:flex-row gap-8 sm:gap-10 text-lg font-semibold pt-2 items-start w-full  ${showMenu ? 'block' : 'hidden sm:block'}`}>
-                <li>
-                    <Link
-                        to="Hero"
-                        spy={true}
-                        smooth={true}
-                        offset={-80}
-                        duration={500}
-                        activeClass="font-bold text-blue-900"
-                        style={{ textDecoration: 'none' }}
-                        onClick={closeMenu}
-                    >
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="Products"
-                        spy={true}
-                        smooth={true}
-                        offset={-80}
-                        duration={500}
-                        activeClass="font-bold"
-                        style={{ textDecoration: 'none' }}
-                        onClick={closeMenu}
-                    >
-                        Products
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="contact"
-                        spy={true}
-                        smooth={true}
-                        offset={-80}
-                        duration={500}
-                        activeClass="font-bold"
-                        style={{ textDecoration: 'none' }}
-                        onClick={closeMenu}
-                    >
-                        Contact Us
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="aboutus"
-                        spy={true}
-                        smooth={true}
-                        offset={-80}
-                        duration={500}
-                        activeClass="font-bold"
-                        style={{ textDecoration: 'none' }}
-                        onClick={closeMenu}
-                    >
-                        About Us
-                    </Link>
-                </li>
-            </ul>
-
-            {/* Social Media Icons - Only visible on larger screens */}
-            <div className="hidden sm:flex items-center gap-4 mt-5">
-                <a href="/">
-                    <img src={InstagramIcon} alt="Instagram" className="h-10 w-10 text-white hover:text-green-500" />
-                </a>
-                <a href="/">
-                    <img src={FacebookIcon} alt="Facebook" className="h-10 w-10 text-white hover:text-green-500" />
-                </a>
-                <a href="/">
-                    <img src={LinkedInIcon} alt="LinkedIn" className="h-10 w-10 text-white hover:text-green-500" />
-                </a>
-            </div>
-        </nav>
-    );
+function Aboutus() {
+  return (
+    <div id="aboutus" className="w-screen  h-auto p-10 sm:p-0 sm:h-[60%] bg-[#F8EDF5] mt-20 flex flex-col sm:flex-row items-center   ">
+      <div className="w-full sm:w-[35%] flex flex-col gap-5 p-6 sm:p-20">
+        <div className="flex w-full items-start justify-center gap-5 text-[#983D7E]">
+          <span className="text-xl text-left w-full font-bold ">
+            About us --------------
+          </span>
+        </div>
+        <p className="text-2xl w-[60%] text-left font-Ls font-bold text-[#15206D]">Lorem ipsum Dummy Text</p>
+        <button className="text-white border  bg-[#15206D] w-[50%] sm:w-[50%] md:w-[50%] px-4 py-2 mr-1 rounded-[12px] hover:bg-[#15206D] hover:text-white transition-colors duration-300">
+          Read More
+        </button>
+      </div>
+      <div className="w-full sm:w-[30%]">
+        <img
+          src={Frame}
+          alt="Email Icon"
+          className="h-[80%] sm:h-full w-full"
+        />
+      </div>
+      <div className="w-full sm:w-[35%] p-6 sm:p-10 ml-4 flex justify-center items-center flex-col text-[#888888]">
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+        <p>
+          It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged.
+        </p>
+      </div>
+    </div>
+  )
 }
 
-export default Navbar;
+export default Aboutus

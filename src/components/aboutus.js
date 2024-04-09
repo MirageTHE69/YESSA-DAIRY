0259531ac3c4e745fd388f8510cefd5a44b5126c
unknown <mthakore12@gmail.com>
2024-04-09 12:10:31 +0530

    adddedd new things

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,9 +1,14 @@
 import React from "react"
 import Frame from "../images/Frame.svg"
+import { navigate } from 'gatsby';
+
 
 function Aboutus() {
+  const handleClick = () => {
+    navigate('/aboutPage');
+};
   return (
-    <div  className="w-screen  h-auto p-10 sm:p-0 sm:h-[60%] bg-[#F8EDF5] mt-20 flex flex-col sm:flex-row items-center   ">
+    <div id="aboutus" className="w-screen  h-auto p-10 sm:p-0 sm:h-[60%] bg-[#F8EDF5] mt-20 flex flex-col sm:flex-row items-center   ">
       <div className="w-full sm:w-[35%] flex flex-col gap-5 p-6 sm:p-20">
         <div className="flex w-full items-start justify-center gap-5 text-[#983D7E]">
           <span className="text-xl text-left w-full font-bold ">
@@ -11,8 +16,8 @@ function Aboutus() {
           </span>
         </div>
         <p className="text-2xl w-[60%] text-left font-Ls font-bold text-[#15206D]">Lorem ipsum Dummy Text</p>
-        <button className="text-white border  bg-[#15206D] w-[50%] sm:w-[50%] md:w-[50%] px-4 py-2 mr-1 rounded-[12px] hover:bg-[#15206D] hover:text-white transition-colors duration-300">
-          Read More
+        <button className="text-white border bg-[#15206D] w-[50%] sm:w-[50%] md:w-[50%] px-4 py-2 mr-1 rounded-[12px] hover:bg-[#15206D] hover:text-white transition-colors duration-300" onClick={handleClick}>
+            Read More
         </button>
       </div>
       <div className="w-full sm:w-[30%]">
